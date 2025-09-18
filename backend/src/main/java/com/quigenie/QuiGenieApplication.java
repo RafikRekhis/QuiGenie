bdcec76441a6441658f54ccb3254ac3dfd552472
REKHIS Rafik <rafik.rekhis@bordeaux-inp.fr>
2025-09-18 14:36:46 +0100

    chore(env): scaffold backend; add env examples, Spring profiles, Angular environments

---
+++ backend/src/main/java/com/quigenie/QuiGenieApplication.java
@@ -0,0 +1,13 @@
+package com.quigenie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuiGenieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuiGenieApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/quigenie/QuiGenieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quigenie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuiGenieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
